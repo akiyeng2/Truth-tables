32fe7286cf97eb930d4ccfa232abacf51c4e0722
Amalan Iyengar <amalan.iyengar@gmail.com>
2013-11-28 14:54:29 -0500

    Deleted old generator, replaced it with new one
    Made it more efficient by not re-shunting it every time, and going
    through the array of tokens as operators so it can be passed directly
    into the reverse polish reader

--- truth.js
+++ truth.js
@@ -29,45 +29,45 @@ function genvalues(num){
 }
 /**
  * Generates a truth table in console
- * 
+ *
  * @param {String} input The string with the truth statement, e.g. "p and x"
  * @param {Integer} num The number of variables
- * @returns null
+ * @returns {Boolean[]} The results of the truth table
  */
-function newtable(input,num){
-	//Tokenizes the input for parsing and replacing
-	input=tokenize(input,false);
+
+function table(input, num){
+	var postfix=shunt(input);
 	vars={};
-	//Initializes the variables in it, from p to z
 	for(var i=112;i<112+num;i++){
-		vars[String.fromCharCode(i)]=1;
+		vars[String.fromCharCode[i]]=1;
 	}
-	//Generate a table with the values of what the variables will be each time you run through it
 	var vals=genvalues(num);
-	
+	var results=[];
 	for(var i=0;i<Math.pow(2,num);i++){
 		var output="";
 		for(var j=0;j<num;j++){
+			//Start at 112, the char code for p, and go forward
 			vars[String.fromCharCode(j+112)]=vals[j][i];
 			output+=!!vals[j][i]+"\t";
 		}
 		//Clone the input as opposed to creating a reference to it
-		newinput=input.slice(0);
+		newinput=JSON.parse(JSON.stringify(postfix));
 
 		for(var z=0;z<newinput.length;z++){
-			var token=newinput[z];
-			
+			var token=newinput[z].name;
+			//Check if the token is a variable, if it is, replace it with its value
 			if((token.length===1) && !(token=="(" ||token==")")){
 				//Replace "p", or "q", with their respective values
-				newinput[z]=vars[newinput[z]];
+				newinput[z].name=vars[newinput[z].name];
 
 
 			}
 		}
 		//Apply the shunting algorithm and evaluate it
-		var result=!!RPN(shunt(newinput.join(" ")));
-		console.log(output+result);
+		var result=!!RPN(newinput);
 
+		results.push(result);
 		
 	}
+	return results;
 }
